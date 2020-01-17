6b557c1564cb2206deb3d2cff4012b5ebc2afc3a
Johannes Schneemann <hannes.schneemann@gmail.com>
2020-01-16 20:09:19 -0600

    created default files

---
+++ Snowman-Cam/Snowman-CamTests/Snowman_CamTests.swift
@@ -0,0 +1,34 @@
+//
+//  Snowman_CamTests.swift
+//  Snowman-CamTests
+//
+//  Created by Johannes Schneemann on 1/16/20.
+//  Copyright © 2020 Johannes Schneemann. All rights reserved.
+//
+
+import XCTest
+@testable import Snowman_Cam
+
+class Snowman_CamTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
